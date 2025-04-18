862f618566c20afd3597b7f815ab590f9759856f
ashcherbina.itrum <ashcherbina.itrum@gmail.com>
2025-04-18 13:52:43 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>React Quiz</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
